52bb84671687ef0c8e8d45339a33686b698e3cd4
Abdu Ibrahim <84140657+ibbe199@users.noreply.github.com>
2026-03-09 21:42:23 +0100

    Create links.js

---
+++ links.js
@@ -0,0 +1,13 @@
+const links = {
+
+earbuds: "https://amzn.to/4lh1nZr",
+smartwatch: "https://amzn.to/4d7byxw",
+ssd: "https://amzn.to/40T35qo",
+headphones: "https://amzn.to/4ud1BoF",
+speaker: "https://amzn.to/40SpLXM",
+webcam: "https://amzn.to/3Nc6Tjs",
+powerbank: "https://amzn.to/46M2FFY",
+smarthub: "https://amzn.to/47kGcQm",
+controller: "https://amzn.to/4renAZn"
+
+};
