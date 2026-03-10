1321d9d803c2ae3ce3fb64e27428ada471557fe2
Abdu Ibrahim <84140657+ibbe199@users.noreply.github.com>
2026-03-10 14:58:12 +0100

    Update links.js

--- links.js
+++ links.js
@@ -1,14 +1,33 @@
 const links = {
-https://www.amazon.com/hz/mobile/mission?_encoding=UTF8&p=l8lgZu%2FzlWnt2d4LjvsnaDjUpiaf0risM0OXG%2FqxZ3O1INV3rH5MDVL5X6ch7TbjZYfKbI766fYgIFHmBVy3TzdEv8ZvNmanHHDTznRu0ctrE%2FqDhs8PG9zpGws8jHl9BLEAvADcvSAg6CH3yIona7xdKE6UUGb9xxFd2ZZc67BFoV4d0lwKu8oMbSiahUTB4pV%2BUbAcoTyaKKTBhrExqoB9e%2BZ%2FH9W%2Buwt1m3rPT2J%2BB5PkGVJ7Xz3YeZk9ulriZwEXhkeKPW562R4XzxxZumfFN6vcxV5KV1Ng8W0o2z6k3l5hSZq0vlFlcHF%2FzRrqLIcn5AFfdGdKcQYxdMaKMgQ6I7WiPO6U06kEUnEV6BPubiSInU%2BWzI7aWUIVQAStJaEy7Txjouq%2F2iKD9DlZaQrTREikHJzyT4VOcj728FPoA9x1eV%2BETA%3D%3D&pd_rd_w=pvSXb&content-id=amzn1.sym.1a792c30-891a-427d-8308-0b0836965822%3Aamzn1.symc.30b24d23-b616-4a3e-8d24-433d43a7339c&pf_rd_p=1a792c30-891a-427d-8308-0b0836965822&pf_rd_r=J8HEJDA3YN30NET66C2D&pd_rd_wg=7RG7g&pd_rd_r=ef2e492f-8e4b-439c-8796-4e6767c07d89&linkCode=ll2&tag=no05c2b-20&linkId=672f0ee803069613357f19b7ede01291&language=en_US&ref_=as_li_ss_tl
-earbuds: "https://amzn.to/4lh1nZr",
-smartwatch: "https://amzn.to/4d7byxw",
-ssd: "https://amzn.to/40T35qo",
-headphones: "https://amzn.to/4ud1BoF",
-speaker: "https://amzn.to/40SpLXM",
-webcam: "https://amzn.to/3Nc6Tjs",
-powerbank: "https://amzn.to/46M2FFY",
-smarthub: "https://amzn.to/47kGcQm",
-controller: https://www.amazon.com/mh?_encoding=UTF8&s=B0FRLJ3YVM%2CB0D7VG4754&pd_rd_w=DtXQc&content-id=amzn1.sym.53d12ada-4284-4a8d-a949-9ac105c857ab%3Aamzn1.symc.0f9275f4-0db9-41b1-8798-6db32f75748e&pf_rd_p=53d12ada-4284-4a8d-a949-9ac105c857ab&pf_rd_r=4CEP1S9ZKYN0PBR8H5VB&pd_rd_wg=PyvUB&pd_rd_r=b466ab61-0a82-4c12-a24f-30c5c661599f&linkCode=ll2&tag=no05c2b-20&linkId=e2c381c36b6e85295e38b056c1c5d81e&language=en_US&ref_=as_li_ss_tl
-https://www.amazon.com/mh?_encoding=UTF8&s=B0G4SWDNBB%2CB0DPJMTH4N%2CB0DP3CP2SY&pd_rd_w=zuMUw&content-id=amzn1.sym.53d12ada-4284-4a8d-a949-9ac105c857ab%3Aamzn1.symc.0f9275f4-0db9-41b1-8798-6db32f75748e&pf_rd_p=53d12ada-4284-4a8d-a949-9ac105c857ab&pf_rd_r=AK1ZPBEKHVJ7ACQ0PEY5&pd_rd_wg=xBAyC&pd_rd_r=a325fc96-5a5e-4d9d-9005-33e60dfc1aed&linkCode=ll2&tag=no05c2b-20&linkId=50da1dc51223f6d321f64e5f59a8f493&language=en_US&ref_=as_li_ss_tl
-https://www.amazon.com/Display-Processor-Graphics-Windows-AG15-32P-39R2/dp/B0DT7FC9B7?psc=1&linkCode=ll2&tag=no05c2b-20&linkId=677966a9a69fb7586b2d5f266cc9a722&language=en_US&ref_=as_li_ss_tl
+  mission: "https://www.amazon.com/hz/mobile/mission?_encoding=UTF8&p=l8lgZu",
+
+  earbuds: "https://amzn.to/4lh1nZr",
+
+  monitor: "https://www.amazon.com/Coolby-15-6inch-1920x1080-Computer-Charging/dp/B0CRR6YQ6N",
+
+  smartwatch: "https://amzn.to/4d7byxw",
+
+  ssd: "https://amzn.to/40T35qo",
+
+  earbuds2: "https://www.amazon.com/TAGRY-Bluetooth-Headphones-Earphones-Waterproof/dp/B09DT48V16",
+
+  headphones: "https://amzn.to/4ud1BoF",
+
+  iphone: "https://www.amazon.com/Apple-iPhone-Version-Desert-Titanium/dp/B0DPJMTH4N",
+
+  speaker: "https://amzn.to/40SpLXM",
+
+  webcam: "https://amzn.to/3Nc6Tjs",
+
+  ipad: "https://www.amazon.com/Apple-11-inch-Wi-Fi-Cellular-128GB/dp/B0FG8HLRZY",
+
+  powerbank: "https://amzn.to/46M2FFY",
+
+  smarthub: "https://amzn.to/47kGcQm",
+
+  controller: "https://www.amazon.com/mh?_encoding=UTF8&s=B0FRLJ3YVM",
+
+  laptop: "https://www.amazon.com/Display-Processor-Graphics-Windows-AG15-32P-39R2/dp/B0DT7FC9B7",
+
+  smartphone: "https://www.amazon.com/International-Unlocked-T-Mobile-Snapdragon-Smartphone/dp/B0DTHQVYWR"
 };
