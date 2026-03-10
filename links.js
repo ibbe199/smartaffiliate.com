16e5b0df64dbcbfab694ab14880f48111f2b7ca2
Abdu Ibrahim <84140657+ibbe199@users.noreply.github.com>
2026-03-10 14:38:05 +0100

    Update links.js

--- links.js
+++ links.js
@@ -9,5 +9,5 @@ webcam: "https://amzn.to/3Nc6Tjs",
 powerbank: "https://amzn.to/46M2FFY",
 smarthub: "https://amzn.to/47kGcQm",
 controller: "https://amzn.to/4renAZn"
-
+https://www.amazon.com/Display-Processor-Graphics-Windows-AG15-32P-39R2/dp/B0DT7FC9B7?psc=1&linkCode=ll2&tag=no05c2b-20&linkId=677966a9a69fb7586b2d5f266cc9a722&language=en_US&ref_=as_li_ss_tl
 };
