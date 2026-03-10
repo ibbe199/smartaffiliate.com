179de70f0d7475026026697810263b31419edac0
Abdu Ibrahim <84140657+ibbe199@users.noreply.github.com>
2026-03-10 15:33:20 +0100

    Update links.js

--- links.js
+++ links.js
@@ -1,16 +1,57 @@
 const links = {
-  mission: "https://www.amazon.com/hz/mobile/mission?_encoding=UTF8&p=l8lgZu",
-  earbuds: "https://amzn.to/4lh1nZr",
-  monitor: "https://www.amazon.com/dp/B0CRR6YQ6N?tag=no05c2b-20",
-  smartwatch: "https://amzn.to/4d7byxw",
-  ssd: "https://amzn.to/40T35qo",
-  headphones: "https://www.amazon.com/dp/B09DT48V16?tag=no05c2b-20",
-  iphone: "https://www.amazon.com/dp/B0DPJMTH4N?tag=no05c2b-20",
-  speaker: "https://amzn.to/40SpLXM",
-  webcam: "https://amzn.to/3Nc6Tjs",
-  ipad: "https://www.amazon.com/dp/B0FG8HLRZY?tag=no05c2b-20",
-  powerbank: "https://amzn.to/46M2FFY",
-  smarthub: "https://amzn.to/47kGcQm",
-  laptop: "https://www.amazon.com/dp/B0DT7FC9B7?tag=no05c2b-20",
-  smartphone: "https://www.amazon.com/dp/B0DTHQVYWR?tag=no05c2b-20"
+  mission: [
+    "https://www.amazon.com/hz/mobile/mission?_encoding=UTF8&p=l8lgZu%2FzlWnt2d4LjvsnaDjUpiaf0risM0OXG%2FqxZ3O1INV3rH5MDVL5X6ch7TbjZYfKbI766fYgIFHmBVy3TzdEv8ZvNmanHHDTznRu0ctrE%2FqDhs8PG9zpGws8jHl9BLEAvADcvSAg6CH3yIona7xdKE6UUGb9xxFd2ZZc67BFoV4d0lwKu8oMbSiahUTB4pV%2BUbAcoTyaKKTBhrExqoB9e%2BZ%2FH9W%2Buwt1m3rPT2J%2BB5PkGVJ7Xz3YeZk9ulriZwEXhkeKPW562R4XzxxZumfFN6vcxV5KV1Ng8W0o2z6k3l5hSZq0vlFlcHF%2FzRrqLIcn5AFfdGdKcQYxdMaKMgQ6I7WiPO6U06kEUnEV6BPubiSInU%2BWzI7aWUIVQAStJaEy7Txjouq%2F2iKD9DlZaQrTREikHJzyT4VOcj728FPoA9x1eV%2BETA%3D%3D"
+  ],
+
+  earbuds: [
+    "https://amzn.to/4lh1nZr",
+    "https://www.amazon.com/Coolby-15-6inch-1920x1080-Computer-Charging/dp/B0CRR6YQ6N"
+  ],
+
+  smartwatch: [
+    "https://amzn.to/4d7byxw"
+  ],
+
+  ssd: [
+    "https://amzn.to/40T35qo",
+    "https://www.amazon.com/TAGRY-Bluetooth-Headphones-Earphones-Waterproof/dp/B09DT48V16"
+  ],
+
+  headphones: [
+    "https://amzn.to/4ud1BoF",
+    "https://www.amazon.com/Apple-iPhone-Version-Desert-Titanium/dp/B0DPJMTH4N"
+  ],
+
+  speaker: [
+    "https://amzn.to/40SpLXM"
+  ],
+
+  webcam: [
+    "https://amzn.to/3Nc6Tjs",
+    "https://www.amazon.com/Apple-11-inch-Wi-Fi-Cellular-128GB/dp/B0FG8HLRZY"
+  ],
+
+  powerbank: [
+    "https://amzn.to/46M2FFY"
+  ],
+
+  smarthub: [
+    "https://amzn.to/47kGcQm"
+  ],
+
+  controller: [
+    "https://www.amazon.com/mh?_encoding=UTF8&s=B0FRLJ3YVM%2CB0D7VG4754"
+  ],
+
+  mixed: [
+    "https://www.amazon.com/mh?_encoding=UTF8&s=B0G4SWDNBB%2CB0DPJMTH4N%2CB0DP3CP2SY"
+  ],
+
+  laptop: [
+    "https://www.amazon.com/Display-Processor-Graphics-Windows-AG15-32P-39R2/dp/B0DT7FC9B7"
+  ],
+
+  smartphone: [
+    "https://www.amazon.com/International-Unlocked-T-Mobile-Snapdragon-Smartphone/dp/B0DTHQVYWR"
+  ]
 };
