08a6443f334dd33c8be2d93990eb49e5e9608eb8
Abdu Ibrahim <84140657+ibbe199@users.noreply.github.com>
2026-03-10 15:02:35 +0100

    Update links.js

--- links.js
+++ links.js
@@ -1,33 +1,16 @@
 const links = {
   mission: "https://www.amazon.com/hz/mobile/mission?_encoding=UTF8&p=l8lgZu",
-
   earbuds: "https://amzn.to/4lh1nZr",
-
-  monitor: "https://www.amazon.com/Coolby-15-6inch-1920x1080-Computer-Charging/dp/B0CRR6YQ6N",
-
+  monitor: "https://www.amazon.com/dp/B0CRR6YQ6N?tag=no05c2b-20",
   smartwatch: "https://amzn.to/4d7byxw",
-
   ssd: "https://amzn.to/40T35qo",
-
-  earbuds2: "https://www.amazon.com/TAGRY-Bluetooth-Headphones-Earphones-Waterproof/dp/B09DT48V16",
-
-  headphones: "https://amzn.to/4ud1BoF",
-
-  iphone: "https://www.amazon.com/Apple-iPhone-Version-Desert-Titanium/dp/B0DPJMTH4N",
-
+  headphones: "https://www.amazon.com/dp/B09DT48V16?tag=no05c2b-20",
+  iphone: "https://www.amazon.com/dp/B0DPJMTH4N?tag=no05c2b-20",
   speaker: "https://amzn.to/40SpLXM",
-
   webcam: "https://amzn.to/3Nc6Tjs",
-
-  ipad: "https://www.amazon.com/Apple-11-inch-Wi-Fi-Cellular-128GB/dp/B0FG8HLRZY",
-
+  ipad: "https://www.amazon.com/dp/B0FG8HLRZY?tag=no05c2b-20",
   powerbank: "https://amzn.to/46M2FFY",
-
   smarthub: "https://amzn.to/47kGcQm",
-
-  controller: "https://www.amazon.com/mh?_encoding=UTF8&s=B0FRLJ3YVM",
-
-  laptop: "https://www.amazon.com/Display-Processor-Graphics-Windows-AG15-32P-39R2/dp/B0DT7FC9B7",
-
-  smartphone: "https://www.amazon.com/International-Unlocked-T-Mobile-Snapdragon-Smartphone/dp/B0DTHQVYWR"
+  laptop: "https://www.amazon.com/dp/B0DT7FC9B7?tag=no05c2b-20",
+  smartphone: "https://www.amazon.com/dp/B0DTHQVYWR?tag=no05c2b-20"
 };
