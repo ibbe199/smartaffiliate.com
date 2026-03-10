e9aee299d5bc38110e0bd8fa672d007c8b2815b9
Abdu Ibrahim <84140657+ibbe199@users.noreply.github.com>
2026-03-10 14:40:36 +0100

    Update links.js

--- links.js
+++ links.js
@@ -1,5 +1,5 @@
 const links = {
-
+https://www.amazon.com/hz/mobile/mission?_encoding=UTF8&p=l8lgZu%2FzlWnt2d4LjvsnaDjUpiaf0risM0OXG%2FqxZ3O1INV3rH5MDVL5X6ch7TbjZYfKbI766fYgIFHmBVy3TzdEv8ZvNmanHHDTznRu0ctrE%2FqDhs8PG9zpGws8jHl9BLEAvADcvSAg6CH3yIona7xdKE6UUGb9xxFd2ZZc67BFoV4d0lwKu8oMbSiahUTB4pV%2BUbAcoTyaKKTBhrExqoB9e%2BZ%2FH9W%2Buwt1m3rPT2J%2BB5PkGVJ7Xz3YeZk9ulriZwEXhkeKPW562R4XzxxZumfFN6vcxV5KV1Ng8W0o2z6k3l5hSZq0vlFlcHF%2FzRrqLIcn5AFfdGdKcQYxdMaKMgQ6I7WiPO6U06kEUnEV6BPubiSInU%2BWzI7aWUIVQAStJaEy7Txjouq%2F2iKD9DlZaQrTREikHJzyT4VOcj728FPoA9x1eV%2BETA%3D%3D&pd_rd_w=pvSXb&content-id=amzn1.sym.1a792c30-891a-427d-8308-0b0836965822%3Aamzn1.symc.30b24d23-b616-4a3e-8d24-433d43a7339c&pf_rd_p=1a792c30-891a-427d-8308-0b0836965822&pf_rd_r=J8HEJDA3YN30NET66C2D&pd_rd_wg=7RG7g&pd_rd_r=ef2e492f-8e4b-439c-8796-4e6767c07d89&linkCode=ll2&tag=no05c2b-20&linkId=672f0ee803069613357f19b7ede01291&language=en_US&ref_=as_li_ss_tl
 earbuds: "https://amzn.to/4lh1nZr",
 smartwatch: "https://amzn.to/4d7byxw",
 ssd: "https://amzn.to/40T35qo",
